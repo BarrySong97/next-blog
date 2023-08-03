b0ddea2e13f3e45acbcc0ea8cb77f4a800db9c0d
BarrySong97 <524000659@qq.com>
2023-08-03 16:49:53 +0800

    fix(修復route.ts): 添加一個前置條件

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,18 +1,20 @@
 import { proxy } from "@/blogapi/core/OpenAPI";
 import axios from "axios";
 import { NextResponse } from "next/server";
+export const revalidate = 0;
 
 export async function GET(request: Request) {
   try {
-    // const { searchParams } = new URL(request.url);
-    // const isRecent = searchParams.get("recent") === "true";
+    const { searchParams } = new URL(request.url);
+    const isRecent = searchParams.get("recent") === "true";
+    
+    const response = await axios.get(
+      `${proxy}/api/posts${isRecent ? "/recent" : ""}`
+    );
+    
+    const data = response.data;
 
-    // const response = await axios.get(
-    //   `${proxy}/api/posts${isRecent ? "/recent" : ""}`
-    // );
-    // const data = response.data;
-
-    return NextResponse.json({ a: 1 });
+    return NextResponse.json(data);
   } catch (error) {
     // return NextResponse.json(error);
   }
